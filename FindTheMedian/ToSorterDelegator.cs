35afb34a9216d546977d574339164145b18b8d42
HeaDCraBee <theheadcrabz@gmail.com>
2021-09-26 16:02:18 +0300

    Add sorterDelegator property

---
+++ FindTheMedian/ToSorterDelegator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace FindTheMedian
+{
+    public class ToSorterDelegator
+    {
+        private List<long> _firstList = new List<long>();
+        private List<long> _secondList = new List<long>();
+        private List<long> _result = new List<long>();
+
+        public ToSorterDelegator(List<long> firstList, List<long> secondList)
+        {
+            _firstList.AddRange(firstList);
+            _secondList.AddRange(secondList);          
+        }
+
+        public List<long> GetResult()
+        {
+            return _result;
+        }
+
+        public void Delegate()
+        {
+            _result = Sorter.MergeSort(_firstList, _secondList);
+        }
+    }
+}
